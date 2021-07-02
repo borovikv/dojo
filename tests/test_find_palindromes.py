bc98138c8d61d8d94a40e2c28a32b668a13ed67b
vladimir borovic <vladimir.borovic@ellation.com>
2021-07-02 12:48:55 +0300

    Add tests for count_palindromes

--- tests/test_find_palindromes.py
+++ tests/test_find_palindromes.py
@@ -6,14 +6,18 @@ import find_palindromes as subject
 
 
 @pytest.mark.parametrize(
-    'string,expected', [
-        ('popop', 9),
-        ('pop' * 3_000, 500000500000),
+    'method,string,expected,time_expected', [
+        (subject.count_palindromes, 'popop', 9, 0.01),
+        (subject.count_palindromes, 'pop' * 500, 376250, 20),
+
+        (subject.count_palindromes_n2, 'popop', 9, 0.01),
+        (subject.count_palindromes_n2, 'pop' * 500, 376250, 1),
     ],
-    ids=['popop', 'performance']
+    ids=['popop', 'performance', 'popop_n2', 'performance_n2']
 )
-def test_count_palindromes(string, expected):
+def test_count_palindromes(method,string,expected,time_expected):
     start = time.time()
-    assert expected == subject.count_palindromes(string)
+    assert expected == method(string)
     end = time.time()
-    assert end - start <= 10
+    print(end - start)
+    assert end - start <= time_expected
